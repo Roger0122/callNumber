69c51779b5f478553199cec3b5e2d1fb74669b54
黃俊龍 <a86527913@gmail.com~>
2025-07-17 17:16:02 +0800

    新增 等待人數

--- src/components/TitleBox.vue
+++ src/components/TitleBox.vue
@@ -1,5 +1,6 @@
 <template>
-  <div :class="['text-white py-3 px-4 mb-4 rounded flex items-center  justify-center gap-3', bgColor]">
+  <div :class="[
+  'text-white py-3 px-4 mb-4 rounded flex items-center  justify-center gap-3', bgColor]">
     <!-- 圖示 -->
     <img v-if="icon" :src="icon" alt="icon" class="w-12 h-12" />
 
@@ -8,6 +9,14 @@
       <p class="text-3xl font-bold leading-tight">{{ title }}</p>
       <p class="text-xl font-light text-center">{{ subtitle }}</p>
     </div>
+
+
+    <!-- 右側：等待人數 -->
+    <div v-if="title === '準備中'" class="text-center text-black  ">
+      <p class="text-xl">等待人數</p>
+      <p class="text-3xl ">{{ 0 }}</p>
+    </div>
+
   </div>
 </template>
 
@@ -22,6 +31,6 @@ defineProps({
   bgColor: {
     type: String,
     default: 'bg-gray-600'
-  }
+  },
 })
 </script>
