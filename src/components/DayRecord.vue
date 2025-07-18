d4d56817955eca859b648224e9514308bad836b4
黃俊龍 <a86527913@gmail.com~>
2025-07-18 17:52:28 +0800

    修正  UI調整

--- src/components/DayRecord.vue
+++ src/components/DayRecord.vue
@@ -3,7 +3,7 @@
   <!-- 背景遮罩 -->
     <div class=" absolute inset-0 bg-gray-400 opacity-40"></div>
     <!-- 展示內容 -->
-    <div class="relative bg-white  rounded shadow-lg w-[800px] z-10">
+    <div class="relative bg-white  rounded shadow-lg h-screen w-[600px] z-10">
       <h2 class="text-2xl font-bold mb-4 p-2 bg-black text-white text-center">今日紀錄</h2>
       
       <div class="flex">
@@ -19,20 +19,22 @@
       </div>
     </div>
       </div>
+
       <!-- 選項操作區 -->
     <div class="grid grid-cols-2 px-6 py-2">
       <div
         v-for="item in orderSources"
         :key="item.key"
         class="flex items-center justify-between border rounded shadow-sm">
-        <span class="font-bold text-lg">{{ item.label }}</span>
+        <span class="font-bold text-xl">{{ item.label }}</span>
+
           <div class="bg-amber-600">
-            <div class="border px-4 py-2">
+            <div class="border px-2 py-1 font-bold">
               <button
-                class="rounded text-white"
+                class="rounded text-white text-2xl" 
                 @click="toggleSource(item.key, true)"
               >
-                還原
+                還 原
               </button>
             </div>
           </div>
@@ -42,7 +44,7 @@
 
       <div class="flex justify-center gap-3 mb-4">
         <button class=" font-semibold text-2xl px-8 py-2 border-2 border-gray-600 text-gray-600 rounded-3xl" @click="cancel">取消</button>
-        <button class=" font-semibold text-2xl px-8 py-2  border-2 border-gray-600 text-gray-600 rounded-3xl" @click="confirm">確認</button>
+        <button class=" font-semibold text-2xl px-8 py-2  border-2 border-gray-600 text-gray-600 rounded-3xl" @click="confirm">確定</button>
       </div>
     </div>
   </div>
--- src/components/SystemSettings.vue
+++ src/components/SystemSettings.vue
@@ -3,7 +3,7 @@
   <!-- 背景遮罩 -->
     <div class=" absolute inset-0 bg-gray-400 opacity-40"></div>
     <!-- 展示內容 -->
-    <div class="relative bg-white  rounded shadow-lg w-[800px] z-10">
+    <div class="relative bg-white  rounded shadow-lg h-screen w-[600px] z-10">
       <h2 class="text-2xl font-bold mb-4 p-2 bg-black text-white text-center">用餐別顯示</h2>
       <p class= "text-xl  font-bold ml-6 p-2 bg-green-600 text-white text-center w-30  rounded-3xl">餐別顯示</p> 
     <!-- 選項操作區 -->
