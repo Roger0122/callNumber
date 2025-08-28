7ad5cd8aac5548503d219313956012f8e2d75317
黃俊龍 <a86527913@gmail.com~>
2025-08-28 10:45:29 +0800

    修正 訂單間隔

--- src/components/OrderNumberGrid.vue
+++ src/components/OrderNumberGrid.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="text-2xl font-bold bg-white rounded-md ">
+<div class="text-2xl font-bold bg-white rounded-md  mt-1">
   <div :class="['grid gap-3', {
     'grid-cols-1': columns === 1,
     'grid-cols-2': columns === 2,
